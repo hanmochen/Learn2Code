5a9ed68a4d2034d413daedd9ae7d6758b64d75b3
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-07-26 17:59:05 +0800

    Updated to Lecture 14 CS193P
    Add File Save in project EmojiArt
    Hanmo Chen 2019/07/26
    Changes to be committed:
    	modified:   Swift/CS193P/EmojiArt/EmojiArt.xcodeproj/project.pbxproj
    	modified:   Swift/CS193P/EmojiArt/EmojiArt.xcodeproj/project.xcworkspace/xcuserdata/chm.xcuserdatad/UserInterfaceState.xcuserstate
    	modified:   Swift/CS193P/EmojiArt/EmojiArt/Base.lproj/Main.storyboard
    	new file:   Swift/CS193P/EmojiArt/EmojiArt/EmojiArt.swift
    	modified:   Swift/CS193P/EmojiArt/EmojiArt/EmojiArtView.swift
    	modified:   Swift/CS193P/EmojiArt/EmojiArt/EmojiArtViewController.swift
    	modified:   Swift/CS193P/EmojiArt/EmojiArt/Info.plist
    	renamed:    Swift/CS193P/EmojiArt/EmojiArt/EmojiArtDocumentTableViewController.swift -> Swift/CS193P/EmojiArt/EmojiArt/Junk/EmojiArtDocumentTableViewController.swift
    	renamed:    Swift/CS193P/EmojiArt/EmojiArt/EmojiArtView+Gestures.swift -> Swift/CS193P/EmojiArt/EmojiArt/Junk/EmojiArtView+Gestures.swift
    	renamed:    Swift/CS193P/EmojiArt/EmojiArt/EmojiCollectionViewCell.swift -> Swift/CS193P/EmojiArt/EmojiArt/Junk/EmojiCollectionViewCell.swift
    	renamed:    Swift/CS193P/EmojiArt/EmojiArt/TextFieldCollectionViewCell.swift -> Swift/CS193P/EmojiArt/EmojiArt/Junk/TextFieldCollectionViewCell.swift
    	renamed:    Swift/CS193P/EmojiArt/EmojiArt/Utilities.swift -> Swift/CS193P/EmojiArt/EmojiArt/Junk/Utilities.swift

--- Swift/CS193P/EmojiArt/EmojiArt/EmojiArtViewController.swift
+++ Swift/CS193P/EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -8,7 +8,73 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate,UIScrollViewDelegate,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate{
+extension EmojiArt.EmojiInfo
+{
+    init?(label: UILabel) {
+        if let attributedText = label.attributedText, let font = attributedText.font {
+            x = Int (label.center.x)
+            y = Int (label.center.y)
+            text = attributedText.string
+            size = Int (font.pointSize)
+        } else {
+            return nil
+        }
+    }
+}
+
+
+
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate,UIScrollViewDelegate,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate
+{
+    //MARK: - Model
+    var emojiArt: EmojiArt? {
+        get {
+            if let url = emojiArtBackgroundImage.url {
+                let emojis = emojiArtView.subviews.compactMap {$0 as? UILabel} .compactMap { EmojiArt.EmojiInfo(label: $0)}
+                return EmojiArt(url: url, emojis: emojis)
+            }
+            return nil
+        }
+        set {
+            emojiArtBackgroundImage = (nil, nil)
+            emojiArtView.subviews.compactMap { $0 as? UILabel } .forEach { $0.removeFromSuperview() }
+            if let url = newValue?.url {
+                imageFetcher = ImageFetcher(fetch: url) { (url, image) in
+                    DispatchQueue.main.async {
+                        self.emojiArtBackgroundImage = (url, image)
+                        newValue?.emojis.forEach {
+                            let attributedText = $0.text.attributedString(withTextStyle: .body, ofSize: CGFloat($0.size))
+                            self.emojiArtView?.addLabel(with: attributedText , centeredAt: CGPoint(x: $0.x, y: $0.y))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        if let json = emojiArt?.json {
+            if var url = try? FileManager.default.url(
+                for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            ) {
+                do {
+                    url.appendPathComponent("Untitled.json")
+                    try json.write(to: url)
+                    print("Saved Successfully")
+                    print(url)
+                } catch let error {
+                    print("couldn't save \(error)")
+                }
+            }
+        }
+    }
+    
     // MARK: - Storyboard
  
     @IBOutlet weak var dropZone: UIView!{
@@ -40,14 +106,17 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate,UIScro
         return emojiArtView
     }
     
-    var emojiArtBackgroundImage: UIImage? {
+    private var _emojiArtBackgroundImageURL: URL?
+    
+    var emojiArtBackgroundImage: (url: URL?,image: UIImage?) {
         get {
-            return emojiArtView.backgroundImage
+            return (_emojiArtBackgroundImageURL,emojiArtView.backgroundImage)
         }
         set {
+            _emojiArtBackgroundImageURL = newValue.url
             scrollView?.zoomScale = 1.0
-            emojiArtView.backgroundImage = newValue
-            let size = newValue?.size ?? CGSize.zero
+            emojiArtView.backgroundImage = newValue.image
+            let size = newValue.image?.size ?? CGSize.zero
             emojiArtView.frame = CGRect(origin: CGPoint.zero, size: size)
             scrollView?.contentSize = size
             scrollViewHeight?.constant = size.height
@@ -221,7 +290,7 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate,UIScro
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession){
         imageFetcher = ImageFetcher(handler: { (url, image) in
             DispatchQueue.main.async {
-                self.emojiArtBackgroundImage = image
+                self.emojiArtBackgroundImage = (url, image)
             }
         })
         
